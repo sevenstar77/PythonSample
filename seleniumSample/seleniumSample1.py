fa9725791e8689c4fde568994f562211a91f1345
DESKTOP-R0989KT\sh <amazonprimeday777@gmail.com>
2017-10-03 01:26:51 +0900

    selenium

---
+++ seleniumSample/seleniumSample1.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+driver = webdriver.Chrome('c:\\git\\auth\\chromedriver')
+driver.implicitly_wait(3)
+driver.get('https://nid.naver.com/nidlogin.login')
+
+driver.find_element_by_name('id').send_keys('your ID')
+driver.find_element_by_name('pw').send_keys('your password')
+
+#driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+
+driver.get('https://section.cafe.naver.com/')
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
