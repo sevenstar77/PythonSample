088deebd937d58199208d37c307f02a2e7f6d5f6
sevenstar77 <amazonprimeday777@gmail.com>
2017-10-23 13:45:53 +0900

    pymmsql modify

--- pymssqlSample.py
+++ pymssqlSample.py
@@ -1,18 +1,33 @@
 import pymssql
 
 conn = pymssql.connect(host='your mssql server ip', user='', password='', database='')
-curs = conn.cursor()
-sql = 'query ~'
-curs.execute(sql)
-rows = curs.fetchall() #data
-desc = curs.description #colums name
+def executeQuery(query):
+    curs = conn.cursor()
+    sql = 'query ~'
+    curs.execute(sql)
 
+    rows = curs.fetchall() #data
+    desc = curs.description #colums name
 
-for row in rows:
-    columns = []
-    for col in desc:
-        columns.append(col[0])
-    #print(dict(zip(columns, row)))
-    dataDict = dict(zip(columns, row)) #  colums, row to dic
+    result = []
+    for row in rows:
+        columns = []
+        for col in desc:
+            columns.append(col[0])
+        #print(dict(zip(columns, row)))
+        dataDict = dict(zip(columns, row)) #  colums, row to dic
+        for key, val in dataDict.items():
+            if str(type(val)) == "<class 'decimal.Decimal'>":
+                dataDict[key] = float(val)
+            if str(type(val)) == "<class 'datetime.datetime'>":
+                dataDict[key] = str(val)
+        result.append(dataDict)
+    return result
+
+def updateQuery(query):
+
+    with conn.cursor() as cursor:
+        cursor.exeucte(query)
+        conn.commit()
 
 
