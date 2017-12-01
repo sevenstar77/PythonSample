252146990b1c6421ebf3cfd836546cee28caae8d
1 <1@1ui-Mac-mini.local>
2017-12-01 21:48:19 +0900

    csvSample add

---
+++ science/csvSample.py
@@ -0,0 +1,20 @@
+import codecs
+
+file_name = "test1.csv"
+listData = []
+
+
+csv = codecs.open(file_name, "r", "utf-8").read()
+print("csv", csv)
+rows = csv.split('\n')
+
+for row in rows:
+    if rows == "":
+        continue
+    cells = row.split(",")
+    listData.append(cells)
+
+#print(listData)
+
+for c in listData:
+    print(c[1], c[2], c[3])
